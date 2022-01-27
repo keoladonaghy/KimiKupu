336ed4ed200fd70081b9f2049d8fca3d04016966
keoladonaghy <donaghy@hawaii.edu>
2022-01-27 12:54:44 -1000

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
     tries: 6, // This changes how many tries you get to finish the wordle
-    language: "‘Ōlelo Hawai‘i", // This changes the display name for your language
+    language: "Hawaiian", // This changes the display name for your language
     wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
     author: "keoladonaghy", // Put your name here so people know who made this Wordle!
-    authorWebsite: "keoladonaghy.com", // Put a link to your website or social media here
-    wordListSource: "keoladonagny", // Describe the source material for your words here
-    wordListSourceLink: "keoladonaghy", // Put a link to the source material for your words here
+    authorWebsite: "http://keoladonaghy.com", // Put a link to your website or social media here
+    wordListSource: "keoladonaghy", // Describe the source material for your words here
+    wordListSourceLink: "http://keoladonaghy.com", // Put a link to the source material for your words here
     //
     // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
     //
