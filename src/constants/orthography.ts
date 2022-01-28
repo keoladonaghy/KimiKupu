057ab31b79b15297468a08d8ea8fa8b5b52f731e
keoladonaghy <donaghy@hawaii.edu>
2022-01-27 20:04:09 -1000

    0127222004
    s

--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -1,6 +1,6 @@
 import { CONFIG } from "./config";
 
-export const ORTHOGRAPHY = ['a', 'ā', 'e', 'ē', 'i', 'ī', 'o', 'ō', 'u', 'ū', '‘'];
+export const ORTHOGRAPHY = ['a', 'ā', 'e', 'ē', 'i', 'ī', 'o', 'ō', 'u', 'ū','h', 'k', 'l', 'm', 'n', 'p', 'w','‘'];
 
 if (CONFIG.normalization) {
     ORTHOGRAPHY.forEach((val, i) => ORTHOGRAPHY[i] = val.normalize(CONFIG.normalization))
