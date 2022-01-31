e2c904361b9486af4c0e077943b7d47ad426a396
keoladonaghy <donaghy@hawaii.edu>
2022-01-30 16:24:40 -1000

    012922 1624

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -69,36 +69,36 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="W" status="correct" />
-                      <Cell value="E" />
-                      <Cell value="A" />
-                      <Cell value="R" />
-                      <Cell value="Y" />
-                    </div>
-                    <p className="text-sm text-gray-500">
-                      The letter W is in the word and in the correct spot.
-                    </p>
-
-                    <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="P" />
+                      <Cell value="L" status="correct" />
+                      <Cell value="Ā" />
+                      <Cell value="H" />
+                      <Cell value="U" />
                       <Cell value="I" />
-                      <Cell value="L" status="present" />
-                      <Cell value="O" />
-                      <Cell value="T" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter L is in the word but in the wrong spot.
+                      The letter L is in the word and in the correct spot.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="V" />
                       <Cell value="A" />
-                      <Cell value="G" />
-                      <Cell value="U" status="absent" />
-                      <Cell value="E" />
+                      <Cell value="L" />
+                      <Cell value="O" status="present" />
+                      <Cell value="H" />
+                      <Cell value="A" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter U is not in the word in any spot.
+                      The letter O is in the word but in the wrong spot.
+                    </p>
+
+                    <div className="flex justify-center mb-1 mt-4">
+                      <Cell value="‘" />
+                      <Cell value="Ā" />
+                      <Cell value="I" />
+                      <Cell value="N" status="absent" />
+                      <Cell value="A" />
+                    </div>
+                    <p className="text-sm text-gray-500">
+                      The letter N is not in the word in any spot.
                     </p>
                   </div>
                 </div>
