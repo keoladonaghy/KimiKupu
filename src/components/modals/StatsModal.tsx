460ad20fedcbe7be30dbe6245e59912ee0f967a4
keoladonaghy <donaghy@hawaii.edu>
2022-02-09 15:31:41 -1000

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -40,7 +40,7 @@ export const StatsModal = ({
       </h4>
       <Histogram gameStats={gameStats} />
       {(isGameLost || isGameWon) && (
-        <div className="mt-5 sm:mt-6 columns-1">
+        <div className="mt-5 sm:mt-6 columns-3">
           <div>
             <a
               className="underline font-medium"
