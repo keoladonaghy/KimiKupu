7a4aed1deefe17acd23374f154337f8ab89873c9
keoladonaghy <donaghy@hawaii.edu>
2025-08-28 21:04:00 -1000

    Add/Update config and orthography files

---
+++ src/constants/orthography.mao.ts
@@ -0,0 +1,30 @@
+import { CONFIG } from './config'
+
+export const ORTHOGRAPHY = [
+  'a',
+  'ā',
+  'e',
+  'ē',
+  'i',
+  'ī',
+  'o',
+  'ō',
+  'u',
+  'ū',
+  'h',
+  'k',
+  'm',
+  'n',
+  'ng',  
+  'p',
+  'r',
+  'w',
+  'wh',
+  'ʻ',
+]
+
+if (CONFIG.normalization) {
+  ORTHOGRAPHY.forEach(
+    (val, i) => (ORTHOGRAPHY[i] = val.normalize(CONFIG.normalization))
+  )
+}
